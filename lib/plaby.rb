d7c2cb325139bd95dfbe4cacf81c0df511ff92c2
Michael Trommer <mictro@gmail.com>
2015-05-18 20:14:21 +0200

    initial

---
+++ lib/plaby.rb
@@ -0,0 +1,22 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+module Plaby
+
+  VERSION = [0, 0, 1]
+
+  Source  = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+
+end
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
